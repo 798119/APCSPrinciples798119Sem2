d0d3097bc90456a06f22862db8f92c3c5934bcde
798119 <54591234+798119@users.noreply.github.com>
2020-01-30 08:38:30 -0800

    f

--- Art One/Boids.js
+++ Art One/Boids.js
@@ -1,19 +1,53 @@
+//boid class
 
-//boids class
+class Boid{
+  constructor(x, y, dx, dy){
+    this.w=200;
+    this.clr = color(random(0), (100), random(255))
+    this.loc = createVector(x, y);
+    this.vel = createVector(dx, dy);
+    this.acc = createVector(0,0)
 
-class Boids{
-  constructor(x,y,dx,dy) {
-    this.loc=createVector(x,y);
-    this.vel=createVector(dx,dy);
-    this.acc=createVector(.01,.03);
-    this.clr(0,0,100);
-  }
 }
-
-
 
 run(){
-  this.checkedges();
-  this.update();
   this.render();
+  this.checkEdges();
+  this.update();
 }
+
+render(){
+  var distance;
+  for(var i = 0; i < boids.length; i++){
+    distance = this.loc.dist(boids[i].loc)
+    if(distance < 700){
+    stroke(this.clr)
+      line(this.loc.x, this.loc.y, boids[i].loc.x, boids[i].loc.y)
+
+    }
+
+  }
+
+}
+
+  checkEdges(){
+    if(this.loc.x < 0 || this.loc.x > width){
+      this.vel.x = -this.vel.x;
+    }
+    if(this.loc.y < 0 || this.loc.y > height){
+      this.vel.y = -this.vel.y;
+    }
+
+  }
+
+
+
+update(){
+  this.vel.limit(50);
+  this.loc.add(this.vel);
+  this.vel.add(this.acc);
+
+}
+
+
+}//end boids
--- Art One/sketch.js
+++ Art One/sketch.js
@@ -1,18 +1,36 @@
-//  Your Name
+//  calvin akrpal
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
+var boids = []
+
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(5, 5, 5);
-  fill(200, 30, 150);
+  background(0, 255, 255);
+  loadBoids(60);
 
+}
+//  The draw function is called @ 30 fps
+function draw() {
+  background(0,50,60);
+  runBoids();
 
 }
 
-//  The draw function is called @ 30 fps
-function draw() {
-  background(0,0,0);
-  runBoids();
+function loadBoids(n){
+    for(var i =0; i < n; i++){
+      boids[i] = new Boid(random(width), random(height), random(-4, 2), random(-4, 3));
+
+  }
+
+}
+
+function runBoids(){
+    for(var i = 0; i < boids.length; i++){
+      boids[i].run();
+  }
+
+
+
 }
