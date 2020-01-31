1a6203013ee8f1e35f25096de42daf55ebc1105f
798119 <54591234+798119@users.noreply.github.com>
2020-01-31 08:48:59 -0800

    f

---
+++ Art Two/box.js
@@ -0,0 +1,7 @@
+
+
+
+  class Rect(x,y){
+
+
+  }
---
+++ Art Two/sketch.js
@@ -0,0 +1,37 @@
+//  calvin akrpal
+// 	Date or version number
+//  This is a comment
+//  The setup function function is called once when your program begins
+var boids = []
+
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(0, 255, 255);
+  loadBoids(5);
+
+
+}
+//  The draw function is called @ 30 fps
+function draw() {
+  background(0,50,60);
+  runBoids();
+
+}
+
+function loadBoids(n){
+    for(var i =0; i < n; i++){
+      boids[i] = new Boid((width/2), random(height), random(-4, 2), random(-4, 3));
+
+  }
+
+}
+
+function runBoids(){
+    for(var i = 0; i < boids.length; i++){
+      boids[i].run();
+  }
+
+
+
+}
