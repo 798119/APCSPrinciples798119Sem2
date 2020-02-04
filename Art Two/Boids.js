4d9c714688b28275824f52f38fe6a1a605bd03ee
798119 <54591234+798119@users.noreply.github.com>
2020-02-03 21:11:42 -0800

    s

--- Art Two/Boids.js
+++ Art Two/Boids.js
@@ -1,49 +1,76 @@
-//boid class
-
 class Boid{
-  constructor(x, y, dx, dy){
-    this.w=50;
-    this.clr = color(random(0), (100), random(255))
+  constructor(x, y, dx, dy, id){
+    this.w=45;
+    this.clr = color(255, 55,255)
     this.loc = createVector(x, y);
     this.vel = createVector(dx, dy);
+    this.id = id;
     this.acc = createVector(0,0)
+      if(this.id<0){
+          this.w=25;
+    }
+      else{
+        this.w = 15
+    }
 }
-
 run(){
   this.render();
   this.checkEdges();
   this.update();
 }
+  render(){
+   if(this.id < 0){
+   fill(this.clr)
+   ellipse(this.loc.x, this.loc.y, this.w, this.w);
+}
 
-render(){
+else{
+
+
   var distance;
+
   for(var i = 0; i < boids.length; i++){
     distance = this.loc.dist(boids[i].loc)
-    if(distance < 700){
-    stroke(this.clr)
+    if(distance < this.w + boids[i].w && distance > 0){
+      stroke(this.clr)
       line(this.loc.x, this.loc.y, boids[i].loc.x, boids[i].loc.y)
-
     }
   }
 }
-
+}
   checkEdges(){
-    if(this.loc.x < 0 || this.loc.x > width){
-      this.vel.x = -this.vel.x;
+    if(this.loc.x < 0){
+      this.loc.x = width;
     }
-    if(this.loc.y < 0 || this.loc.y > height){
-      this.vel.y = -this.vel.y;
+    if(this.loc.x > width){
+      this.loc.x = 0;
     }
-
+    if(this.loc.y < 0){
+      this.loc.y = height;
+    }
+    if(this.loc.y > height){
+      this.loc.y = 0;
+    }
   }
-
-
 update(){
-  this.vel.limit(50);
+  var distToMainBoid
+  if(this.id >= 0){
+    distToMainBoid = this.loc.dist(mainBoid.loc);
+    if(distToMainBoid < 800){
+      this.acc = p5.Vector.sub(mainBoid.loc, this.loc);
+      this.acc.normalize();
+      this.acc.mult(.07);
+    }
+    if(distToMainBoid < 150){
+      this.acc = p5.Vector.sub(this.loc, mainBoid.loc);
+      this.acc.normalize();
+      this.acc.mult(.5);
+    }
+  }
+  this.vel.limit(2);
   this.loc.add(this.vel);
   this.vel.add(this.acc);
 
-}
+}//end of update
 
-
-}//end boids
+}//end of ball class
--- Art Two/sketch.js
+++ Art Two/sketch.js
@@ -3,30 +3,31 @@
 //  This is a comment
 //  The setup function function is called once when your program begins
 var boids = []
-
+var mainBoid =[]
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(0, 255, 255);
-  loadBoids(5);
-
-
+  background(100, 200, 20,);
+  loadObjects(400);
 }
 //  The draw function is called @ 30 fps
 function draw() {
-  background(0,50,60);
-  runBoids();
+  background(100,0,50,80);
+  runObjects();
+
 }
 
-function loadBoids(n){
+function loadObjects(n){
+  mainBoid = new Boid(width/4, height/4, random(-2, 1), random(-1, 2), -1);
     for(var i =0; i < n; i++){
-      boids[i] = new Boid((width/2), random(height), random(-4, 2), random(-4, 3));
+      boids[i] = new Boid(random(width), random(height), random(-3, 2), random(-3, 2), i);
+  }
+}
 
-      }
-    }
-
-function runBoids(){
+function runObjects(){
+  mainBoid.run();
     for(var i = 0; i < boids.length; i++){
       boids[i].run();
   }
+
 }
